adcf6a01f87183c6a2e28da2f90a7a95b8f08943
lucas.bordini <131024143+wzlucas@users.noreply.github.com>
2025-04-04 15:55:41 -0300

    Add files via upload

---
+++ Links.php
@@ -0,0 +1,43 @@
+<?php 
+
+class Link {
+    
+    private $linkImg;
+    private $info;
+
+    /**
+     * Get the value of linkImg
+     */
+    public function getLinkImg()
+    {
+        return $this->linkImg;
+    }
+
+    /**
+     * Set the value of linkImg
+     */
+    public function setLinkImg($linkImg): self
+    {
+        $this->linkImg = $linkImg;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of info
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+
+    /**
+     * Set the value of info
+     */
+    public function setInfo($info): self
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+}
